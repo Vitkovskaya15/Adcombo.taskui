91a4bb4c95f5c98c0642e4fbfa04e54ea4395a1b
Marisha <ms.marina.vitkovskaya@mail.ru>
2020-03-22 13:49:24 +0300

    добавлен ui для создания задачи

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TaskAddComponent } from './task-add/task-add.component';  
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {  
+    path: 'task/create',  
+    component: TaskAddComponent  
+  },  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/task-add/task-add.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';  
+
+@Component({
+  selector: 'app-task-add',
+  templateUrl: './task-add.component.html',
+  styleUrls: ['./task-add.component.css']
+})
+export class TaskAddComponent implements OnInit {
+  angForm: FormGroup;  
+
+  constructor(private fb: FormBuilder) {  
+    this.createForm();  
+  } 
+
+  createForm() {  
+    this.angForm = this.fb.group({  
+      TaskName: ['', Validators.required ],  
+      TaskDescription: ['', Validators.required ],  
+      TaskPriority: ['', Validators.required ]  
+    });  
+  }  
+
+  ngOnInit(): void {
+  }
+
+}
